d716c5e1521b18de86b5a52ba2ea02a8a79c2517
aloneinthedork <alex.aloneinthedork@gmail.com>
2020-02-01 21:37:59 +0100

    Pushback para el jugador

---
+++ Assets/Scripts/PushPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 direction;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //are you Player?
+        if (other.gameObject.tag == "Player")
+        {
+            //fuck off then
+            GameObject player = other.gameObject;
+            var heading = player.transform.position - transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance; //normalized object-player direction
+
+            other.gameObject.GetComponent<MovementControl>().Pushback(direction);
+
+        }
+    }
+}
