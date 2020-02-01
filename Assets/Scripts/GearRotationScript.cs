8b6e4bfc6494687d85561f9077b5c0b5961d9dd7
Humberto Parisi <parisi.jose@gmail.com>
2020-02-01 15:41:14 +0100

    Script de cerebro y prefabs

---
+++ Assets/Scripts/GearRotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearRotationScript : MonoBehaviour
+{
+    public float speed=0;
+    public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, rotation, speed * Time.deltaTime);
+    }
+}
