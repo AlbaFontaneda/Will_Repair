966fac234be8dccbb08dffcfde6238072345d15a
AlbaFontaneda <alba.fontaneda@gmail.com>
2020-02-01 03:37:41 +0100

    revert

---
+++ Assets/Scripts/FlashSprite.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashSprite : MonoBehaviour
+{
+    int delay = 2;
+    public SpriteRenderer mySpriteRenderer;
+    int counter;
+    bool flashed = false;
+    bool toggle = false;
+    int deathCounter = 0;
+
+    void FixedUpdate()    // you can you FixedUpdate for fixed flash rate
+    {
+        Flash(mySpriteRenderer);
+
+        if (deathCounter > 6)
+        {
+            mySpriteRenderer.enabled = true;
+            deathCounter = 0;
+            Death death = this.gameObject.GetComponent<Death>();
+            death.Spawn();
+        }
+    }
+
+   public void enabledFlash(bool enable) {
+        flashed = enable;
+    }
+
+
+    void Flash(SpriteRenderer spriteRen)
+    {
+
+        if (flashed)
+        {
+            if (counter >= delay)
+            {
+                deathCounter++;
+                counter = 0;
+
+                toggle = !toggle;
+                if (toggle)
+                {
+                    spriteRen.enabled = true;
+                }
+                else
+                {
+                    spriteRen.enabled = false;
+                }
+
+            }
+            else
+            {
+                counter++;
+            }
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/steamDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class steamDamage : MonoBehaviour
+{
+    private FlashSprite flashSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        GameObject colObject = col.gameObject;
+        if (colObject.gameObject.tag == "Player")
+        {
+            Debug.Log("Matar al nanobot");
+            FlashSprite flashSprite = colObject.GetComponent<FlashSprite>();
+
+            flashSprite.enabledFlash(true);
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        GameObject colObject = col.gameObject;
+        if (colObject.gameObject.tag == "Player")
+        {
+            Debug.Log("Matar al nanobot");
+            FlashSprite flashSprite = colObject.GetComponent<FlashSprite>();
+
+            flashSprite.enabledFlash(false);
+        }
+
+    }
+}
