7289ac1377936a7fc51bdbc54372a1ea978a0001
pacastejon <pacastejon@gmail.com>
2020-02-01 03:05:33 +0100

    mi mierditas

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    /*Uses Thaleah Font from the AssetStore*/
+
+    float timeLeft = 300.0f;
+
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        float seconds = Mathf.Round(timeLeft);
+
+        text.text = ((int)(seconds/60)).ToString().PadLeft(2, '0') + ":"+(seconds%60).ToString().PadLeft(2, '0');
+        if (timeLeft < 0)
+        {
+            Application.LoadLevel("gameOver");
+        }
+    }
+}
