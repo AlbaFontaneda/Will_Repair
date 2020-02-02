fa1a1fa6ac55711cdc6cc332fab3b741abdfb93d
AlbaFontaneda <alba.fontaneda@gmail.com>
2020-02-02 12:57:22 +0100

    menus con controller

---
+++ Assets/ManageMenuButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageMenuButton : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource = null;
+
+    private bool pressStart = false;
+    private bool pressExit = false;
+    private MenuManager mManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mManager = gameObject.GetComponent<MenuManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        pressStart = Input.GetButtonDown("BotonAbajo");
+        pressExit = Input.GetButtonDown("BotonDerecha");
+
+
+        if (pressStart)
+        {
+            _audioSource.Play();
+            mManager.StartGameDelay(0.3f);
+        }
+
+        if (pressExit)
+        {
+            _audioSource.Play();
+            mManager.ExitGameDelay(0.3f);
+        }
+
+    }
+}
